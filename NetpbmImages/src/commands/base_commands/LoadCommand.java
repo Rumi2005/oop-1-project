760e0dcde9d5e2faf6db2f4a652c8bcd410aec8d
RumenK <rumen_kitanov@abv.bg>
2026-05-15 19:08:40 +0300

    Added the creation of an image on loading if it doesn't exist

--- NetpbmImages/src/commands/base_commands/LoadCommand.java
+++ NetpbmImages/src/commands/base_commands/LoadCommand.java
@@ -18,7 +18,14 @@ public class LoadCommand implements Command {
     @Override
     public String execute() {
         try {
-            Image image = Utilities.load(path);
+            Image image;
+            File file = new File(path);
+            if (file.exists())
+                image = Utilities.load(path);
+            else {
+                image = createImage(path);
+                Utilities.saveImage(image, path);
+            }
             Session session = new Session(image, path);
             SessionManager.setCurrentSession(session);
             return "Session with ID: " + session.getId() + " started\nImage '" + path + "' added";
@@ -26,5 +33,25 @@ public class LoadCommand implements Command {
         } catch (IOException e) {
             return "Failed to load image: " + e.getMessage();
         }
+    }
+
+    private Image createImage(String path) throws IOException {
+        String lower = path.toLowerCase();
+        String magic;
+        if(lower.endsWith(".pbm"))
+            magic = "P1";
+        else if(lower.endsWith(".pgm"))
+            magic = "P2";
+        else if(lower.endsWith(".ppm"))
+            magic = "P3";
+        else
+            throw new IOException("Unsupported file type.");
+        Image image = Utilities.imageFactory.get(magic).get();
+        image.setWidth(1);
+        image.setHeight(1);
+        if (!magic.equals("P1"))
+            image.setMaxVal(255);
+        image.setData(new int[image.getWidth() * image.getHeight() * image.getChannels()]);
+        return image;
     }
 }
